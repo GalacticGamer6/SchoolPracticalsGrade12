01b120a200ce7f88f8f0f009861d52787efdf3ff
GalacticGamer6 <nereaav.ranjit@reddam.house>
2023-03-03 12:17:42 +0200

    kk

---
+++ src/main/java/JSON/Student.java
@@ -0,0 +1,19 @@
+package JSON;
+
+public class Student {
+    
+    private String name;
+    private int age;
+    private String strength;
+    
+    public Student(String n, int a, String s){
+        
+        name = n;
+        age = a;
+        strength = s;
+        
+    }
+    
+    
+    
+}
---
+++ src/main/java/JSON/StudentManager.java
@@ -0,0 +1,19 @@
+package JSON;
+
+public class StudentManager {
+    
+    Student [] arr = new Student[50];
+    private int size;
+    
+    
+    //
+    public StudentManager(){
+        
+        String file = "data//"
+        Scanner sc = new Scanner()
+        
+    }
+    
+    
+    
+}
